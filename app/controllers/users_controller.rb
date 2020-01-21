fecd1708bef3c652a80af0119f5abc7478332488
hmalyala <m.hemanthkumar27@gmail.com>
2020-01-21 10:33:25 -0500

    Update Gemfile for postgres instead of sqlite3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            session[:user_id] = @user.id
             redirect_to @user,
             notice: "Thanks for Signing up!"
         else
